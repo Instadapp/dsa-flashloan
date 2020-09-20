81a05115a6e3df570bfd4afedf179e86e798bcef
Thrilok Kumar <thrilok2000@gmail.com>
2020-09-21 02:42:26 +0530

    Added dydx flashloanBase contract

---
+++ contracts/flashloan/dydx/DydxFlashloanBase.sol
@@ -0,0 +1,200 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+interface Account {
+    struct Info {
+        address owner; // The address that owns the account
+        uint256 number; // A nonce that allows a single address to control many accounts
+    }
+}
+
+interface Actions {
+    enum ActionType {
+        Deposit, // supply tokens
+        Withdraw, // borrow tokens
+        Transfer, // transfer balance between accounts
+        Buy, // buy an amount of some token (publicly)
+        Sell, // sell an amount of some token (publicly)
+        Trade, // trade tokens against another account
+        Liquidate, // liquidate an undercollateralized or expiring account
+        Vaporize, // use excess tokens to zero-out a completely negative account
+        Call // send arbitrary data to an address
+    }
+
+    struct ActionArgs {
+        ActionType actionType;
+        uint256 accountId;
+        Types.AssetAmount amount;
+        uint256 primaryMarketId;
+        uint256 secondaryMarketId;
+        address otherAddress;
+        uint256 otherAccountId;
+        bytes data;
+    }
+
+    struct DepositArgs {
+        Types.AssetAmount amount;
+        Account.Info account;
+        uint256 market;
+        address from;
+    }
+
+    struct WithdrawArgs {
+        Types.AssetAmount amount;
+        Account.Info account;
+        uint256 market;
+        address to;
+    }
+
+    struct CallArgs {
+        Account.Info account;
+        address callee;
+        bytes data;
+    }
+}
+
+interface Types {
+    enum AssetDenomination {
+        Wei, // the amount is denominated in wei
+        Par // the amount is denominated in par
+    }
+
+    enum AssetReference {
+        Delta, // the amount is given as a delta from the current value
+        Target // the amount is given as an exact number to end up at
+    }
+
+    struct AssetAmount {
+        bool sign; // true if positive
+        AssetDenomination denomination;
+        AssetReference ref;
+        uint256 value;
+    }
+
+    struct Wei {
+        bool sign; // true if positive
+        uint256 value;
+    }
+}
+
+
+interface ISoloMargin {
+    struct OperatorArg {
+        address operator;
+        bool trusted;
+    }
+
+    function getMarketTokenAddress(uint256 marketId)
+        external
+        view
+        returns (address);
+
+    function getNumMarkets() external view returns (uint256);
+
+
+    function operate(
+        Account.Info[] calldata accounts,
+        Actions.ActionArgs[] calldata actions
+    ) external;
+
+    function getAccountWei(Account.Info calldata account, uint256 marketId)
+        external
+        view
+        returns (Types.Wei memory);
+}
+
+contract DydxFlashloanBase {
+    function _getMarketIdFromTokenAddress(address _solo, address token)
+        internal
+        view
+        returns (uint256)
+    {
+        ISoloMargin solo = ISoloMargin(_solo);
+
+        uint256 numMarkets = solo.getNumMarkets();
+
+        address curToken;
+        for (uint256 i = 0; i < numMarkets; i++) {
+            curToken = solo.getMarketTokenAddress(i);
+
+            if (curToken == token) {
+                return i;
+            }
+        }
+
+        revert("No marketId found for provided token");
+    }
+
+    function _getAccountInfo() internal view returns (Account.Info memory) {
+        return Account.Info({owner: address(this), number: 1});
+    }
+
+    function _getWithdrawAction(uint marketId, uint256 amount)
+        internal
+        view
+        returns (Actions.ActionArgs memory)
+    {
+        return
+            Actions.ActionArgs({
+                actionType: Actions.ActionType.Withdraw,
+                accountId: 0,
+                amount: Types.AssetAmount({
+                    sign: false,
+                    denomination: Types.AssetDenomination.Wei,
+                    ref: Types.AssetReference.Delta,
+                    value: amount
+                }),
+                primaryMarketId: marketId,
+                secondaryMarketId: 0,
+                otherAddress: address(this),
+                otherAccountId: 0,
+                data: ""
+            });
+    }
+
+    function _getCallAction(bytes memory data)
+        internal
+        view
+        returns (Actions.ActionArgs memory)
+    {
+        return
+            Actions.ActionArgs({
+                actionType: Actions.ActionType.Call,
+                accountId: 0,
+                amount: Types.AssetAmount({
+                    sign: false,
+                    denomination: Types.AssetDenomination.Wei,
+                    ref: Types.AssetReference.Delta,
+                    value: 0
+                }),
+                primaryMarketId: 0,
+                secondaryMarketId: 0,
+                otherAddress: address(this),
+                otherAccountId: 0,
+                data: data
+            });
+    }
+
+    function _getDepositAction(uint marketId, uint256 amount)
+        internal
+        view
+        returns (Actions.ActionArgs memory)
+    {
+        return
+            Actions.ActionArgs({
+                actionType: Actions.ActionType.Deposit,
+                accountId: 0,
+                amount: Types.AssetAmount({
+                    sign: true,
+                    denomination: Types.AssetDenomination.Wei,
+                    ref: Types.AssetReference.Delta,
+                    value: amount
+                }),
+                primaryMarketId: marketId,
+                secondaryMarketId: 0,
+                otherAddress: address(this),
+                otherAccountId: 0,
+                data: ""
+            });
+    }
+}
