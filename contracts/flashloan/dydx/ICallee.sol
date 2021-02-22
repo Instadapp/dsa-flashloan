2247d12a661e2d2ef1f5d0c071d6b6d8c8e1b26e
yashnaman <shahyashs145@gmail.com>
2021-02-22 18:55:14 +0530

    remove the dependency of calling only cast method on a dsa to allow general call (any calls)

--- contracts/flashloan/dydx/ICallee.sol
+++ contracts/flashloan/dydx/ICallee.sol
@@ -1,4 +1,12 @@
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+interface Account {
+    struct Info {
+        address owner; // The address that owns the account
+        uint256 number; // A nonce that allows a single address to control many accounts
+    }
+}
 
 /**
  * @title ICallee
@@ -7,7 +15,6 @@ pragma solidity ^0.6.0;
  * Interface that Callees for Solo must implement in order to ingest data.
  */
 interface ICallee {
-
     // ============ Public Functions ============
 
     /**
@@ -21,6 +28,5 @@ interface ICallee {
         address sender,
         Account.Info calldata accountInfo,
         bytes calldata data
-    )
-        external;
+    ) external;
 }
