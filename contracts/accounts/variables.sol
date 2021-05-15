bc2334fff678cecdbbdcb2b33ff3a76d0829f095
Thrilok Kumar <thrilok2000@gmail.com>
2021-05-16 03:22:58 +0530

    Added comment

--- contracts/accounts/variables.sol
+++ contracts/accounts/variables.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.7.0;
 contract Variables {
     // Auth Module(Address of Auth => bool).
     mapping(address => bool) internal _auth;
-    uint256 internal _status = 1;
+    // Reentrancy Guard variable
+    uint256 internal _status;
 }
