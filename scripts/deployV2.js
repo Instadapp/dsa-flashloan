f8a7a65175410f2622ef1cdc3da2eb4493151f97
Thrilok Kumar <thrilok2000@gmail.com>
2021-11-01 23:55:01 +0530

    updated deployment script

--- scripts/deployV2.js
+++ scripts/deployV2.js
@@ -4,7 +4,7 @@ const { deploy } = deployments;
 
 async function main() {
     const deployer = (await getUnnamedAccounts())[0]
-    let INSTA_MASTER_PROXY;
+    let INSTA_MASTER_PROXY_ADMIN;
     let INSTA_INDEX;
     let AAVE_LENDING
     let W_CHAIN_TOKEN
@@ -13,7 +13,7 @@ async function main() {
         console.log(
             "\n\n Deploying Contracts to mainnet. Hit ctrl + c to abort"
         );
-        INSTA_MASTER_PROXY = "0xa471D83e526B6b5D6c876088D34834B44D4064ff"
+        INSTA_MASTER_PROXY_ADMIN = "0xa8c31E39e40E6765BEdBd83D92D6AA0B33f1CCC5"
         INSTA_INDEX = "0x2971AdFa57b20E5a416aE5a708A8655A9c74f723"
         AAVE_LENDING = "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
         W_CHAIN_TOKEN = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
@@ -27,7 +27,7 @@ async function main() {
         console.log(
           "\n\n Deploying Contracts to matic..."
         );
-        INSTA_MASTER_PROXY = "0x697860CeE594c577F18f71cAf3d8B68D913c7366"
+        INSTA_MASTER_PROXY_ADMIN = "0x90cF378A297C7eF6dabeD36eA5E112c6646BB3A4"
         INSTA_INDEX = "0xA9B99766E6C676Cf1975c0D3166F96C0848fF5ad"
         AAVE_LENDING = "0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf"
         W_CHAIN_TOKEN = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"
@@ -43,15 +43,17 @@ async function main() {
       INSTA_MASTER = "0x3A4A2540Df5aB4Ef5503bbfBFec31ce7474B056f"
   }
 
-    const instaPoolV2ImplementationV2 = await deploy("InstaPoolV2ImplementationV2", {
-        from: deployer,
-        args: [INSTA_INDEX, W_CHAIN_TOKEN, AAVE_LENDING]
-    })
+
+  const instaAdminProxy = await deploy("InstaAdminProxy", {
+      from: deployer,
+      args: [INSTA_MASTER_PROXY_ADMIN]
+  })
+  console.log("InstaAdminProxy deployed: ", instaAdminProxy.address);
     console.log("InstaPoolV2Implementation deployed: ", instaPoolV2ImplementationV2.address);
 
     const instaPoolV2 = await deploy("InstaPoolV2", {
         from: deployer,
-        args: [instaPoolV2ImplementationV2.address, INSTA_MASTER_PROXY, "0x"]
+        args: [instaPoolV2ImplementationV2.address, instaAdminProxy.address, "0x"]
     })
     console.log("InstaPoolV2 deployed: ", instaPoolV2.address);
     
@@ -61,7 +63,14 @@ async function main() {
     await InstaPoolV2Proxy.initialize(sigs, INSTA_MASTER)
 
     if (hre.network.name === "mainnet" || hre.network.name === "kovan" || hre.network.name == "matic") {
-        await hre.run("verify:verify", {
+      await hre.run("verify:verify", {
+        address: instaAdminProxy.address,
+        constructorArguments: [INSTA_MASTER_PROXY_ADMIN],
+        contract: "contracts/proxy/proxyAdmin.sol:InstaAdminProxy"
+      }
+    )  
+      
+      await hre.run("verify:verify", {
             address: instaPoolV2ImplementationV2.address,
             constructorArguments: [INSTA_INDEX, W_CHAIN_TOKEN, AAVE_LENDING]
           }
@@ -70,7 +79,7 @@ async function main() {
         await hre.run("verify:verify", {
             address: instaPoolV2.address,
             contract: "contracts/proxy/Instapool.sol:InstaPoolV2",
-            constructorArguments: [instaPoolV2ImplementationV2.address, INSTA_MASTER_PROXY, "0x"]
+            constructorArguments: [instaPoolV2ImplementationV2.address, instaAdminProxy.address, "0x"]
           }
         )
 
